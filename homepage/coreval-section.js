142d8b144671a27bc4eef08cd3cdf887d9ebb994
5yeus <silas.owu@amalitech.com>
2023-04-20 11:13:55 -0700

    Core values section for mobile UI done

--- homepage/coreval-section.js
+++ homepage/coreval-section.js
@@ -1,8 +1,67 @@
 document.querySelector('#coreval-section').innerHTML= `
-    <div>
-        <h1>Core Values Section</h1>
-        <p class="read-the-docs">
-            Click on the Vite logo to learn more
-        </p>
+    <div class="mobile:flex mobile:flex-col mobile:overflow-x-hidden mobile:relative">
+        <div class="absolute w-[12.5rem] h-[15.25rem] mobile:overflow-x-hidden mobile:left-[17rem] mobile:ml-[2rem] p-0">
+            <img src='../public/assets/bg-pattern-home-3.svg'/>
+        </div>
+        <div class="mobile:mx-10 mobile:my-[4rem] mobile:gap-y-[3.5rem] mobile:grid mobile:grid-rows-[12.25rem_43.3125rem] mobile:w-[20.4375rem] ">
+            <div class=" mobile:w-[15rem] mobile:flex mobile:flex-col mobile:gap-7">
+                <div class=" bg-light-coral w-[3.125rem] h-[0.25rem]"></div>
+                <h2 class=" font-livvic font-bold text-[2rem] leading-[2rem] text-[#fff]">
+                    Build & <br>
+                    manage <br>
+                    distributed <br>
+                    teams like no <br>
+                    one else.
+                </h2>
+            </div>
+            <div class="flex flex-col gap-y-12">
+                <div class="mobile:flex mobile:flex-col text-[#fff] mobile:mx-auto mobile:gap-4">
+                    <div class="w-[4.5rem] h-[4.5rem] mobile:mx-auto">
+                        <img src='../public/assets/icon-person.svg'/>
+                    </div>
+                    <div>
+                        <h3 class="font-livvic font-bold text-[1.125rem] text-center text-light-coral">Experienced Individuals</h3>
+                        <p class="font-livvic font-semibold text-[0.9375rem] leading-[1.5625rem] text-center mobile:mt-2">
+                            Our network is made up of highly experienced
+                            professionals who are passionate about what
+                            they do
+                        </p>
+                    </div>
+                </div>
+            
+                <div class="mobile:flex mobile:flex-col text-[#fff] mobile:mx-auto mobile:gap-4">
+                    <div class="w-[4.5rem] h-[4.5rem] mobile:mx-auto">
+                        <img src='../public/assets/icon-cog.svg'/>
+                    </div>
+                    <div>
+                        <h3 class="font-livvic font-bold text-[1.125rem] text-center text-light-coral">Easy to Implement</h3>
+                        <p class="font-livvic font-semibold text-[0.9375rem] leading-[1.5625rem] text-center mobile:mt-2">
+                            Our processes have been refined over years of
+                            implementation meaning our teams always 
+                            deliver.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="mobile:flex mobile:flex-col text-[#fff] mobile:mx-auto mobile:gap-4">
+                    <div class="w-[4.5rem] h-[4.5rem] mobile:mx-auto">
+                        <img src='../public/assets/icon-chart.svg'/>
+                    </div>
+                    <div>
+                        <h3 class="font-livvic font-bold text-[1.125rem] text-center text-light-coral">Enhanced Productivity</h3>
+                        <p class="font-livvic font-semibold text-[0.9375rem] leading-[1.5625rem] text-center mobile:mt-2">
+                            Our customized platform with in-built
+                            analytics helps you manage your distrbuted
+                            teams
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-`
+    `;
+
+//  ="">
+//         <-- <div class="w-[12.5rem] h-[15.25rem] relative left-[17.1875rem] top-0">
+//              <img src='../assets/bg-pattern-home-3.svg'/>
+//        </div> -->
