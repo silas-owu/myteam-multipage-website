9e77cd3060bb4ad68e840a4d6b99a1a4be755500
5yeus <silas.owu@amalitech.com>
2023-04-21 12:30:10 -0700

    Core value section in-progress

--- homepage/coreval-section.js
+++ homepage/coreval-section.js
@@ -3,10 +3,10 @@ document.querySelector('#coreval-section').innerHTML= `
         <div class="absolute w-[12.5rem] h-[15.25rem] laptop:hidden tablet:hidden mobile:overflow-x-hidden tablet:right-[-7rem]  tablet:ml-[10rem]  mobile:left-[17rem] mobile:ml-[2rem] ">
             <img src='../assets/bg-pattern-home-3.svg'/>
         </div>
-        <div class="laptop:w-[69.375rem] laptop:h-[25.25rem] laptop:mx-auto laptop:my-[8.75rem] laptop:grid laptop:grid-cols-[12.625rem_21.625rem] laptop:gap-x-[7.8125rem]  mobile:mx-10 mobile:my-[4rem] mobile:gap-y-[3.5rem] mobile:grid mobile:grid-rows-[12.25rem_43.3125rem] mobile:w-[20.4375rem] tablet:w-[35.8125rem] tablet:h-[31.875rem] tablet:grid tablet:grid-rows-[4rem_21.625rem] tablet:mt-[7rem] tablet:gap-y-[6.7rem]">
-            <div class="mobile:w-[15rem] mobile:flex mobile:flex-col mobile:gap-7 tablet:w-[27.8125rem] tablet:h-[4rem]">
+        <div class="laptop:w-[69.375rem] laptop:h-[25.25rem] laptop:mx-auto laptop:my-[8.75rem] laptop:grid laptop:grid-cols-[27.8125rem_33.75rem] laptop:border-2 laptop:border-dashed laptop:border-[#517] laptop:children:border-dashed laptop:children:border-2 laptop:children:border-[#eef] laptop:gap-x-[7.8125rem]  mobile:mx-10 mobile:my-[4rem] mobile:gap-y-[3.5rem] mobile:grid mobile:grid-rows-[12.25rem_43.3125rem] mobile:w-[20.4375rem] tablet:w-[35.8125rem] tablet:h-[31.875rem] tablet:grid tablet:grid-rows-[4rem_21.625rem] tablet:mt-[7rem] tablet:gap-y-[6.7rem]">
+            <div class="mobile:w-[15rem] mobile:flex mobile:flex-col mobile:gap-7 tablet:w-[27.8125rem] tablet:h-[4rem] laptop:h-[12.625rem] laptop:flex laptop:flex-col laptop:gap-y-12">
                 <div class=" bg-light-coral w-[3.125rem] h-[0.25rem]"></div>
-                <h2 class="tablet:mt-8 font-livvic font-bold text-[2rem] leading-[2rem] text-[#fff]">
+                <h2 class="laptop:text-[3rem] laptop:leading-[3rem] laptop:h-[9rem] tablet:text-[2rem] tablet:leading-8 tablet:h-[4rem] tablet:mt-8 font-livvic font-bold mobile:text-[2rem] mobile:leading-[2rem] text-[#fff]">
                     Build &
                     manage 
                     distributed
@@ -14,14 +14,14 @@ document.querySelector('#coreval-section').innerHTML= `
                     one else.
                 </h2>
             </div>
-            <div class="tablet:flex tablet:flex-col  tablet:gap-y-8 mobile:flex mobile:flex-col mobile:gap-y-12">
-                <div class=" tablet:gap-6  mobile:flex mobile:flex-col text-[#fff] mobile:mx-auto mobile:gap-4 tablet:flex tablet:flex-row">
+            <div class="laptop:mt-[3.3rem] laptop:flex laptop:flex-col laptop:gap-y-8 tablet:flex tablet:flex-col  tablet:gap-y-8 mobile:flex mobile:flex-col mobile:gap-y-12 laptop:children:flex laptop:children:flex-row">
+                <div class="gap-6  mobile:flex mobile:flex-col text-[#fff] mobile:mx-auto mobile:gap-4 flex flex-row">
                     <div class="w-[4.5rem] h-[4.5rem] mobile:mx-auto">
                         <img src='../assets/icon-person.svg'/>
                     </div>
-                    <div class="tablet:flex tablet:flex-col tablet:gap-y-4">
-                        <h3 class="tablet:text-left font-livvic font-bold text-[1.125rem] text-center text-light-coral">Experienced Individuals</h3>
-                        <p class="font-livvic font-semibold text-[0.9375rem] leading-[1.5625rem] text-center tablet:leading-[1.5rem] tablet:text-[0.9375rem] tablet:opacity-40 tablet:text-left  mobile:mt-2">
+                    <div class="flex flex-col gap-y-4">
+                        <h3 class="tablet:text-left laptop:text-left font-livvic font-bold text-[1.125rem] mobile:text-center text-light-coral">Experienced Individuals</h3>
+                        <p class="font-livvic font-semibold text-[0.9375rem] leading-[1.5625rem] laptop:text-left mobile:text-center tablet:leading-[1.5rem] tablet:text-[0.9375rem] opacity-40 tablet:text-left  mobile:mt-2">
                             Our network is made up of highly experienced
                             professionals who are passionate about what
                             they do
@@ -29,13 +29,13 @@ document.querySelector('#coreval-section').innerHTML= `
                     </div>
                 </div>
             
-                <div class="tablet:gap-6 mobile:flex mobile:flex-col text-[#fff] mobile:mx-auto mobile:gap-4 tablet:flex tablet:flex-row">
+                <div class="gap-6 mobile:flex mobile:flex-col text-[#fff] mobile:mx-auto mobile:gap-4 flex flex-row">
                     <div class="w-[4.5rem] h-[4.5rem] mobile:mx-auto">
                         <img src='../assets/icon-cog.svg'/>
                     </div>
-                    <div class="tablet:flex tablet:flex-col tablet:gap-y-4">
-                        <h3 class="tablet:text-left font-livvic font-bold text-[1.125rem] text-center text-light-coral">Easy to Implement</h3>
-                        <p class="font-livvic font-semibold text-[0.9375rem] leading-[1.5625rem] tablet:opacity-40 tablet:text-left text-center mobile:mt-2">
+                    <div class="flex flex-col gap-y-4">
+                        <h3 class="laptop:text-left tablet:text-left font-livvic font-bold text-[1.125rem] text-center text-light-coral">Easy to Implement</h3>
+                        <p class="laptop:text-left font-livvic font-semibold text-[0.9375rem] leading-[1.5625rem] opacity-40 tablet:text-left mobile:text-center mobile:mt-2">
                             Our processes have been refined over years of
                             implementation meaning our teams always 
                             deliver.
@@ -43,13 +43,13 @@ document.querySelector('#coreval-section').innerHTML= `
                     </div>
                 </div>
 
-                <div class="tablet:gap-6  mobile:flex mobile:flex-col text-[#fff] mobile:mx-auto mobile:gap-4 tablet:flex tablet:flex-row">
+                <div class="gap-6  mobile:flex mobile:flex-col text-[#fff] mobile:mx-auto mobile:gap-4 flex flex-row">
                     <div class="w-[4.5rem] h-[4.5rem] mobile:mx-auto">
                         <img src='../assets/icon-chart.svg'/>
                     </div>
-                    <div class="tablet:flex tablet:flex-col tablet:gap-y-4">
-                        <h3 class="tablet:text-left font-livvic font-bold text-[1.125rem] text-center text-light-coral">Enhanced Productivity</h3>
-                        <p class="font-livvic font-semibold text-[0.9375rem] leading-[1.5625rem] tablet:opacity-40 mobile:text-center mobile:mt-2">
+                    <div class="flex flex-col gap-y-4">
+                        <h3 class="laptop:text-left tablet:text-left font-livvic font-bold text-[1.125rem] mobile:text-center text-light-coral">Enhanced Productivity</h3>
+                        <p class="font-livvic font-semibold text-[0.9375rem] leading-[1.5625rem] laptop:text-left opacity-40 mobile:text-center mobile:mt-2">
                             Our customized platform with in-built
                             analytics helps you manage your distrbuted
                             teams
