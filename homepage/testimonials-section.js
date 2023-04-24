102e5be5245c57b96c5a8adc7005901935397f54
5yeus <silas.owu@amalitech.com>
2023-04-23 17:29:14 -0700

    Testimonials section mobile UI done

--- homepage/testimonials-section.js
+++ homepage/testimonials-section.js
@@ -1,8 +1,72 @@
 document.querySelector('#testimonials-section').innerHTML= `
-    <div>
-        <h1>Testimonials</h1>
-        <p class="read-the-docs">
-            Click on the Vite logo to learn more
-        </p>
+    <div class="mobile:w-full mobile:flex mobile:flex-col mobile:gap-y-14 mobile:mx-auto">
+        <div class="mobile:w-[9.1875rem] mobile:h-[6.25rem]">
+            <img src='../assets/bg-pattern-home-4-about-3.svg'/>
+        </div>
+        <div class="mobile:flex mobile:flex-col mobile:gap-y-8 mobile:w-[20.4375rem] mobile:h-[69.125rem] mobile:mx-[2rem]">
+            <div class="mobile:h-[8rem]">
+             <h2 class="font-livvic mobile:text-[2rem] mobile:leading-8 mobile:text-center font-bold text-[#fff]">
+                Delivering real results for top companies.
+                Some of our <span class="text-rapture-blue">success stories.</span>
+             </h2>
+            </div>
+            <div class="mobile:flex mobile:flex-col mobile:gap-y-8 mobile:h-[58.125rem] children:h-[17.375rem] ">
+                <div class="mobile:flex mobile:flex-col mobile:h-[17.375rem]">
+                    <div class="w-[4.1875rem] h-[3.5rem] mobile:mx-auto">
+                        <img src='../assets/icon-quotes.svg'/>
+                    </div>
+                    <h3 class="mt-[-1.2rem] text-[#fff] font-livvic font-semibold text-[0.9375rem] leading-[1.5rem] mobile:text-center">
+                        “The team perfectly fit the specialized skill set required.
+                        They focused on the most essential features helping us 
+                        launch the platform eight months faster than planned.”
+                    </h3>
+                    <div class="flex flex-col h-[7.875rem] mx-auto items-center gap-y-1 font-livvic">
+                        <h3 class="text-rapture-blue text-center text-[1.125rem] leading-[1.75rem] font-semibold ">Kady Baker</h3>
+                        <h4 class=" font-livvic font-italic text-[#fff] font-medium leading-[1.125rem] text-[0.825rem] ">Product Manager at Bookmark</h4>
+                        <div class="rounded-full w-[3.875rem] h-[3.875rem] border-solid border-[0.125rem] border-[#c4ffee]">
+                            <img src='../assets/avatar-kady.jpg'class="rounded-full"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="mobile:flex mobile:flex-col mobile:h-[17.375rem]">
+                    <div class="w-[4.1875rem] h-[3.5rem] mobile:mx-auto">
+                        <img src='../assets/icon-quotes.svg'/>
+                    </div>
+                    <h3 class="mt-[-1.2rem] text-[#fff] font-livvic font-semibold text-[0.9375rem] leading-[1.5rem] mobile:text-center">
+                       “We needed to automate our entire onboarding process. 
+                       The team came in and built out the whole journey.
+                       Since going live, user retention has gone through the roof!”
+                    </h3>
+                    <div class="flex flex-col h-[7.875rem] mx-auto items-center gap-y-1 font-livvic">
+                        <h3 class="text-rapture-blue text-center text-[1.125rem] leading-[1.75rem] font-semibold ">Aiysha Resee</h3>
+                        <h4 class="font-livvic font-italic text-[#fff] leading-[1.125rem] text-[0.825rem]">Founder at Manage</h4>
+                        <div class="rounded-full w-[3.875rem] h-[3.875rem] border-solid border-[0.125rem] border-[#c4ffee]">
+                            <img src='../assets/avatar-aiysha.jpg' class="rounded-full"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="mobile:flex mobile:flex-col mobile:h-[17.375rem]">
+                    <div class="w-[4.1875rem] h-[3.5rem] mobile:mx-auto">
+                        <img src='../assets/icon-quotes.svg'/>
+                    </div>
+                    <h3 class="mt-[-1.2rem] text-[#fff] font-livvic font-semibold text-[0.9375rem] leading-[1.5rem] mobile:text-center">
+                        “Amazing. Our team helped us build an app that delivered 
+                        a new experience for hiring a physio.
+                        The launch was an instant success with 100k
+                        downloads in the first month.”
+                    </h3>
+                    <div class="flex flex-col h-[7.875rem] mx-auto items-center gap-y-1 font-livvic">
+                        <h3 class="text-rapture-blue text-center text-[1.125rem] leading-[1.75rem] font-semibold ">Arthur Clarke</h3>
+                        <h4 class="font-livvic font-italic text-[#fff] font-medium leading-[1.125rem] text-[0.825rem] ">Co-founder of Myphysio</h4>
+                        <div class="rounded-full w-[3.875rem] h-[3.875rem] border-solid border-[0.125rem] border-[#c4ffee]">
+                            <img src='../assets/avatar-arthur.jpg' class="rounded-full"/>
+                        </div>
+                    </div>
+                </div>               
+            </div>
+        </div>
+        <div class="w-[12.5rem] h-[6.2rem]  mobile:ml-auto">
+            <img src='../assets/bg-pattern-home-5.svg'/>
+        </div>
     </div>
 `
