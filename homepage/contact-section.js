e90cee763f3d3df1acddd91d2ab4479cdfbb3326
5yeus <silas.owu@amalitech.com>
2023-04-25 16:27:23 -0700

    Footer section for mobile UI in-progress

--- homepage/contact-section.js
+++ homepage/contact-section.js
@@ -1,6 +1,6 @@
 document.querySelector('#contact-section').innerHTML= `
     <div class=" mobile:w-[21.9375rem] mobile:flex mobile:flex-col mobile:gap-y-0 mobile:mt-16 tablet:flex tablet:flex-col tablet:h-[15rem] tablet:gap-y-4  tablet:mt-16 laptop:mt-[4.75rem]">
-        <div class=" mobile:mt-[1rem]   mobile:w-[20.4375rem] mobile:flex mobile:flex-col  mobile:gap-y-4 flex flex-row justify-center items-center mx-[6.0625rem] tablet:gap-[4rem] laptop:w-[57.3125rem] laptop:justify-between laptop:mx-[16.4375rem] w-[35.8125rem]">
+        <div class=" mobile:mt-[1rem] mobile:ml-[5rem]  mobile:w-[12.89rem] mobile:flex mobile:flex-col  mobile:gap-y-4 flex flex-row justify-center items-center mx-[6.0625rem] tablet:gap-[4rem] laptop:w-[57.3125rem] laptop:justify-between laptop:mx-[16.4375rem] w-[35.8125rem]">
             <h2 class="font-livvic font-bold text-[2rem] leading-[2rem] text-center text-sacramento ">Ready to get started?</h2>
             <button class="h-[3rem] w-[9.5625rem] font-livvic font-semibold text-[1.125rem] leading-[1.75rem] text-sacramento mobile:mx-auto rounded-[1.5rem] border-[0.125rem] border-solid border-sacramento" >contact us</button>
         </div>
--- homepage/footer-section.js
+++ homepage/footer-section.js
@@ -1,8 +1,27 @@
 document.querySelector('#footer-section').innerHTML= `
-    <div>
-        <h1>Footer</h1>
-        <p>
-            Click on the Vite logo to learn more
-        </p>
+    <div class="mobile:px-[1.5rem] mobile:py-[4.125rem] mobile:w-[20.4375rem] mobile:gap-y-7 mobile:h-[18.875rem] mobile:border-dashed mobile:border-2 mobile:border-[#5aa] mobile:flex mobile:flex-col">
+        <div class=" mobile:flex mobile:flex-col mobile:gap-y-6">
+            <div class="w-[6rem] h-[1.5rem]">
+                <img src='../assets/logo.svg'/>
+            </div>
+            <div class="mobile:flex mobile:flex-row mobile:justify-between">
+                <h2 class="font-livvic text-[#fff]">home</h2>
+                <h2 class="font-livvic text-[#fff]">about</h2>
+            </div>
+        </div>
+        <div class="mobile:h-[6.25rem] mobile:text-center font-livvic font-semibold text-[0.9375rem] leading-[1.5625rem]">
+            <p  class="opacity-[60%]">987 Hilcrest Lane</p>
+            <p class="opacity-[60%]">Irvine,CA</p>
+            <p class="opacity-[60%]">California 92714</p>
+            <p class="opacity-[60%]" >Call Us: 949-833-7432</p>
+        </div>
+        <div class="mobile:flex mobile:flex-col mobile:gap-y-4">
+            <div class="mobile:flex mobile:flex-row mobile:justify-evenly">
+                <div><img src='../public/assets/icon-facebook.svg'/></div>
+                <div><img src='../public/assets/icon-pinterest.svg'/></div>
+                <div><img src='../public/assets/icon-twitter.svg'/></div>
+            </div>
+            <p class=" opacity-[60%]">Copyright 2020.All Rights Reserved</p>
+        </div>
     </div>
 `
