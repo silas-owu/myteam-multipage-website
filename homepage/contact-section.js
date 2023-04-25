accb0c54f06506b32c1ab93e102a7686d505924d
5yeus <silas.owu@amalitech.com>
2023-04-25 00:03:22 -0700

    Contact section UI for mobile screen done

--- homepage/contact-section.js
+++ homepage/contact-section.js
@@ -1,8 +1,11 @@
 document.querySelector('#contact-section').innerHTML= `
-    <div class="bg-light-coral">
-        <h1>Contact section</h1>
-        <p>
-            Click on the Vite logo to learn more
-        </p>
+    <div class=" mobile:w-[21.9375rem] children:mobile:border-dashed children:mobile:border-2 children:mobile:border-[#ded] mobile:flex mobile:flex-col mobile:gap-y-2 mobile:">
+        <div class="mobile:mt-[1rem] mobile:mt-[5.1875rem mobile:h-[8.5rem] mobile:w-[20.4375rem] mobile:flex mobile:flex-col  mobile:gap-y-4 ">
+            <h2 class="font-livvic font-bold text-[2rem] leading-[2rem] text-center text-sacramento ">Ready to get started?</h2>
+            <button class="h-[3rem] w-[9.5625rem] font-livvic font-semibold text-[1.125rem] leading-[1.75rem] text-sacramento mobile:mx-auto rounded-[1.5rem] border-[0.125rem] border-solid border-sacramento" >contact us</button>
+        </div>
+        <div class="w-[12.5rem]  mobile:ml-[-3rem]">
+            <img src='../assets/bg-pattern-home-6-about-5.svg'/>
+        </div>
     </div>
 `
