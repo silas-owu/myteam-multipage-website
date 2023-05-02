179fc0ccb5d4ea0c0514fb094222fef933cf58c9
5yeus <silas.owu@amalitech.com>
2023-05-02 16:27:14 -0700

    Aboutpage Laptop UI updated

--- aboutpage/about-homepage.js
+++ aboutpage/about-homepage.js
@@ -1,7 +1,7 @@
 document.querySelector("#about-page").innerHTML= `
 
     <div class="flex flex-col  overflow-hidden">
-        <div class="border-2 border-dashed border-[#aea] laptop:mx-[10.3125rem] laptop:mt-[4.5625rem] laptop:h-[20.25rem] laptop:w-[69.375rem] tablet:mt-[4rem] tablet:mx-[2.4375rem]  tablet:flex tablet:gap-y-[5rem] tablet:flex-col tablet:h-[23.75rem] tablet:w-[43.0625rem] mobile:w-[20.44rem] mobile:mx-12 mobile:flex mobile:flex-col mobile:mt-[3rem] mobile:gap-y-20">
+        <div class="border-2 border-dashed border-[#aea] laptop:mx-[10.3125rem] laptop:mt-[4.5625rem] laptop:h-[20.25rem] laptop:w-[69.375rem] laptop:flex laptop:flex-col laptop:gap-y-[7.5rem] tablet:mt-[4rem] tablet:mx-[2.4375rem]  tablet:flex tablet:gap-y-[5rem] tablet:flex-col tablet:h-[23.75rem] tablet:w-[43.0625rem] mobile:w-[20.44rem] mobile:mx-12 mobile:flex mobile:flex-col mobile:mt-[3rem] mobile:gap-y-20">
             <nav class="flex flex-row nav-mobile tablet:items-center">
                 <div class="logosize-mobile">
                     <img src='../assets/logo.svg'/>
@@ -16,13 +16,16 @@ document.querySelector("#about-page").innerHTML= `
                 </div>
             </nav>
 
-            <div class="font-livvic text-[#fff] text-center tablet:flex tablet:flex-col tablet:h-[15rem] tablet:gap-y-[1.7rem] tablet:w-[28.5625rem] tablet:mx-auto mobile:flex mobile:flex-col mobile:gap-y-4">
-                <h1 class=" font-bold text-[2.5rem] leading-[2.5rem]">About</h1>
-                <p class="font-semibold leading-[1.75rem] tablet:h-[8.75rem] tablet:w-[27.5625rem] mobile:w-[20.4375rem]">
-                    We help companies build dynamic teams made up of top global talent.
-                    Using our network of passionate professionals we drive innovation and deliver incredible outcomes. Talented, diverse teams shape the best products and experiences. 
-                    We’ll bring those teams to you.
-                </p>
+            <div class="font-livvic text-[#fff] text-center laptop:flex laptop:flex-row laptop:gap-12 tablet:flex tablet:flex-col  tablet:h-[15rem] tablet:gap-y-[1.7rem] tablet:w-[28.5625rem] tablet:mx-auto mobile:flex mobile:flex-col mobile:gap-y-4">
+                <h1 class="laptop:w-[21.875rem] font-bold text-[2.5rem] leading-[2.5rem]">About</h1>
+
+                <div class="laptop:flex laptop:flex-col">
+                    <p class="font-semibold leading-[1.75rem] laptop:text-left tablet:h-[8.75rem] tablet:w-[27.5625rem] mobile:w-[20.4375rem]">
+                        We help companies build dynamic teams made up of top global talent.
+                        Using our network of passionate professionals we drive innovation and deliver incredible outcomes. Talented, diverse teams shape the best products and experiences. 
+                         We’ll bring those teams to you.
+                    </p>
+                </div>
             </div>
         </div>
         <img class="w-[12.5rem] h-[12.5rem] mobile:ml-[20.4rem]  tablet:ml-[42rem] tablet:mt-[-5.5rem] laptop:hidden" src='../public/assets/bg-pattern-about-1-mobile-nav-1.svg'/>
