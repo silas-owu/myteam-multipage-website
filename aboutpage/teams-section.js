22f8630496b34008f3ea9c99dd23f52bfa913b03
5yeus <silas.owu@amalitech.com>
2023-05-01 21:21:23 -0700

    Teams section tablet UI updated

--- aboutpage/teams-section.js
+++ aboutpage/teams-section.js
@@ -1,12 +1,12 @@
 document.querySelector('#teams-section').innerHTML=`
 
-    <div class=" tablet:overflow-hidden  tablet:flex tablet:flex-col mobile:flex mobile:flex-col mobile:overflow-hidden ">
+    <div class="tablet:overflow-hidden  tablet:flex tablet:flex-col mobile:flex mobile:flex-col mobile:overflow-hidden ">
         <div class="tablet:-ml-[6.5rem] tablet:mr-auto mobile:-mt-[6.8rem] mobile:-ml-[6.5rem] ">
             <img src='../public/assets/bg-pattern-about-2-contact-1.svg'/>
         </div>
-        <div class="tablet:mx-[6.125rem] tablet:flex tablet:flex-col tablet:mt-0 border-dashed border-2 border-[#1aa] tablet:w-[35.8125] mobile:h-[117.875rem] mobile:mx-6  mobile:flex mobile:flex-col mobile:gap-y-4 ">
-            <h2 class="font-livvic text-[#fff] text-[2rem] font-bold text-center ">Meet the directors</h2>
-            <div class="tablet:grid tablet:grid-cols-2 tablet:h-[58.6875rem] tablet:gap-x-4 mobile:flex mobile:flex-col mobile:gap-y-[1.68rem] chldren:w-[20.4375rem] children:h-[17.5625rem] ">
+        <div class="tablet:mx-[6.125rem] tablet:flex tablet:flex-col tablet:gap-y-10 tablet:mt-[-6rem] tablet:w-[35.8125rem] tablet:h-[68.6875rem] mobile:h-[117.875rem] mobile:mx-6  mobile:flex mobile:flex-col mobile:gap-y-4 ">
+            <h2 class="font-livvic text-[#fff] text-[2rem] font-bold text-center mt-[-0.5rem]">Meet the directors</h2>
+            <div class="tablet:grid tablet:grid-cols-2 tablet:h-[58.6875rem] tablet:gap-x-4 tablet:gap-y-2 mobile:flex mobile:flex-col mobile:gap-y-[1.68rem] chldren:w-[20.4375rem] children:h-[17.5625rem] ">
                 <div>
                     <div class="bg-sacramento font-livvic h-[15.8125rem] flex flex-col mobile:teamscontainer ">
                         <img class="avatar" src='../assets/avatar-nikita.jpg'/>
@@ -67,7 +67,34 @@ document.querySelector('#teams-section').innerHTML=`
                     </div>
                 </div>
 
-                <div>
+                
+                <div class="mobile:hidden">
+                    <div class="bg-sacramento font-livvic flex flex-col h-[15.8125rem] mobile:teamscontainer ">
+                        <div class="text-center  mt-8">
+                            <h3 class=" text-rapture-blue avatarText ">Griffin Wise</h3>
+                            <p class="font-italic text-[#fff] w-[14.625rem] mx-auto text-[1rem] font-semibold leading-6">
+                                “Unique perspectives shape unique products, 
+                                 which is what you need to survive these days.”
+                            </p>
+                        </div>
+                        <div class=" flex flex-row mx-auto mt-4 gap-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
+                                <path fill="#FFF" d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+                                <path fill="#FFF" d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"/>
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="avatarplus bg-rapture-blue">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17">
+                            <path fill="##012f34" fill-rule="evenodd" d="M15.01.368l2.122 2.122-6.01 6.01 6.01 6.01-2.122 2.122L9 10.622l-6.01 6.01L.868 14.51 6.88 8.5.87 2.49 2.988.368 9 6.38 15.01.37z"/>
+                        </svg>
+                    </div>
+                </div>
+
+                
+                <div class="tablet:hidden">
                     <div class="bg-sacramento font-livvic  flex flex-col h-[15.8125rem] mobile:teamscontainer ">
                         <img class="avatar" src='../assets/avatar-griffin.jpg'/>
                         <div class="text-center  mobile:mt-4">
@@ -82,13 +109,28 @@ document.querySelector('#teams-section').innerHTML=`
                     </div>
                 </div>
 
-                <div>
+                <div class="mobile:hidden">
+                    <div class="bg-sacramento font-livvic  flex flex-col h-[15.8125rem] mobile:teamscontainer ">
+                        <img class="avatar" src='../public/assets/avatar-aden.jpg'/>
+                        <div class="text-center  mobile:mt-4">
+                            <h3 class=" text-rapture-blue avatarText ">Aden Allen</h3>
+                            <p class="font-italic text-[#fff]">Head of Talent</p>
+                        </div>
+                    </div>
+                    <div class="avatarplus ">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
+                            <path fill="#012F34" fill-rule="evenodd" d="M10 0v5.999L16 6v4h-6v6H6v-6H0V6h6V0h4z"/>
+                        </svg>
+                    </div>
+                </div>
+
+                <div class="tablet:hidden ">
                     <div class="bg-sacramento font-livvic flex flex-col h-[15.8125rem] mobile:teamscontainer ">
                         <div class="text-center  mobile:mt-8">
                             <h3 class=" text-rapture-blue avatarText ">Aden Allan</h3>
                             <p class="font-italic text-[#fff] w-[14.625rem] mx-auto text-[1rem] font-semibold leading-6">
-                            “Empowered teams create truly amazing products.
-                             Set the north star and let them follow it.”
+                                “Empowered teams create truly amazing products.
+                                 Set the north star and let them follow it.”
                             </p>
                         </div>
                         <div class="mobile:flex mobile:flex-row mobile:mx-auto mobile:mt-4 mobile:gap-4">
@@ -98,7 +140,6 @@ document.querySelector('#teams-section').innerHTML=`
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
                                 <path fill="#FFF" d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"/>
                             </svg>
-
                         </div>
                     </div>
                     <div class="avatarplus bg-rapture-blue">
