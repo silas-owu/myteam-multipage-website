2548b904ecd0e86429f3204c961ac0a5c246147f
5yeus <silas.owu@amalitech.com>
2023-05-04 16:31:28 -0700

    Contact page updated

--- contactpage/landing-page.js
+++ contactpage/landing-page.js
@@ -35,14 +35,13 @@ document.querySelector('#landing-page').innerHTML=`
                         </div>                       
                     </div>
                 </div>
-                <form method="POST" class="laptop:w-[33.875rem] laptop:gap-y-6 tablet:h-[26.25rem] font-livvic font-semibold text-[#fff] text-opacity-60 flex flex-col  tablet:gap-y-10 mobile:gap-y-4 mobile:h-[26.25rem] ">
-                    <input type="text" id="name" name="name" placeholder="Name" class=" h-[2.625rem] border-b  bg-none ">
-                    <input type="email" id="email" name="email" placeholder="Email Address" class=" h-[2.625rem] bg-transparent border-[#fff]">
-                    <input type="text" id="company-name" name="company-name" placeholder="Company Name" class=" h-[2.625rem] bg-transparent border-[#fff]">
-                    <input type="text" id="name" name="name" placeholder="Title" class="h-[2.625rem] bg-transparent border-[#fff]">
-                    <textarea type="text" name="message" placeholder="Message" rows=4></textarea>
+                <form onsubmit="sendEmail() reset(); return false;" method="POST" class="laptop:w-[33.875rem] laptop:gap-y-6 tablet:h-[26.25rem] font-livvic font-semibold text-[#fff] text-opacity-60 flex flex-col  tablet:gap-y-10 mobile:gap-y-4 mobile:h-[26.25rem] ">
+                    <input type="text" id="name" name="name" placeholder="Name"required  class=" h-[2.625rem] bg-[#000] bg-opacity-10 p-4 focus:outline-none">
+                    <input type="email" id="email" name="email" placeholder="Email Address"required  class=" h-[2.625rem] bg-[#000] border-[#fff] p-4 bg-opacity-10 focus:outline-none">
+                    <input type="text" id="company-name" name="company-name" placeholder="Company Name" required  class=" h-[2.625rem] bg-[#000] bg-opacity-10 border-[#fff] p-4 focus:outline-none">
+                    <input type="text" id="title" name="name" placeholder="Title" class="h-[2.625rem] bg-[#000] bg-opacity-10 border-[#fff] p-4 focus:outline-none">
+                    <textarea type="text" id="message" name="message" placeholder="Message" rows=4 class="bg-[#000] bg-opacity-10 p-4 focus:outline-none"></textarea>
                     <button class="h-[3rem] w-[6.875rem] bg-[#FFF] rounded-[1.5rem] font-semibold text-[1.125rem] mobile:mt-8 leading-[1.75rem] text-deep-jungle-green border-[0.125rem] border-solid border-[#fff]" type="submit">submit</button>
-    
                 </form>
             </div>  
         </div>
