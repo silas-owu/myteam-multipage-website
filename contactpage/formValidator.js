d371c266d7d3741650be23be39f3e9563039fec8
5yeus <silas.owu@amalitech.com>
2023-05-08 10:39:07 -0700

    Contact page updated

--- contactpage/formValidator.js
+++ contactpage/formValidator.js
@@ -6,19 +6,19 @@ $(function() {
 
     
     $submitBtn.on('click',function(event){
-        // event.preventDefault();
         $titleID.val() === '' ? 
-            $titleID.next('p').length === 0 
-            && $titleID.after('<p class="text-[#F67E7E] font-bold font-italic text-[0.625rem] leading-[0.8125rem]">This field is required</p>').addClass('placeholder-light-coral border-b-light-coral')
+            ( $titleID.next('p').length === 0 
+            && $titleID.after('<p class="text-[#F67E7E] font-bold font-italic text-[0.625rem] leading-[0.8125rem]">This field is required</p>').addClass('placeholder-light-coral border-b-light-coral'),
+            event.preventDefault())
         :$titleID.next('p').removeClass('placeholder-light-coral');
     });
 
     $submitBtn.on('click',function(event){
-        // event.preventDefault();
         $messageID.val() === '' ? 
-            $messageID.next('p').length === 0
-            && $messageID.after('<p class="text-[#F67E7E] font-bold font-italic text-[0.625rem] leading-[0.8125rem]">This field is required</p>').addClass('placeholder-light-coral border-b-light-coral').attr('placeholder', 'Insert your message here')
-        :$messageID.next('p').removeClass('placeholder-light-coral');
+            ( $messageID.next('p').length === 0
+            && $messageID.after('<p class="text-[#F67E7E] font-bold font-italic text-[0.625rem] leading-[0.8125rem]">This field is required</p>').addClass('placeholder-light-coral border-b-light-coral').attr('placeholder', 'Insert your message here'),
+            event.preventDefault())
+        :$messageID.next('p').removeClass('placeholder-light-coral')
     });
 
     
