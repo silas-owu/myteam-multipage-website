09a52279743c308cbaeeb6a46f95a8098b2907f7
5yeus <silas.owu@amalitech.com>
2023-05-11 13:19:10 -0700

    Addteam card implemented and its form in-progress

--- aboutpage/addteam-modal.js
+++ aboutpage/addteam-modal.js
@@ -1,7 +1,13 @@
 $(function(){
     $('#add-btn').on('click',(event) => {
-        const $addteamModal =$('<div class="w-full h-full bg-dark-green bg-opacity-60">stbatr</div>')
-        $('#addteam-modal').append($addteamModal).css('display', 'block');
+        const $addteamModal =$('<div class="w-full h-full bg-dark-green bg-opacity-60 text-[#fff]">stbatr</div>')
+        const $addteamCard = $('<div class=" mx-auto mt-[4rem] bg-police-blue w-[12.5rem] h-[12.5rem] text-[#eab]">This is where you add your details</div>')
+        $addteamModal.append($addteamCard);
+        $('#addteam-modal').append($addteamModal).css({
+            display:'block',
+
+        });
+
     });
     $('#addteam-modal').on('click',(event) => {
         $(event.currentTarget).css('display', 'none');
