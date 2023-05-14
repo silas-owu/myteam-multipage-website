602217fda2a827ae0117f90026e84649a8037f93
5yeus <silas.owu@amalitech.com>
2023-05-13 21:45:47 -0700

    Addteam-modal form/card functionality implemented to teams-container

--- aboutpage/teams-section.js
+++ aboutpage/teams-section.js
@@ -24,11 +24,19 @@ document.querySelector('#teams-section').innerHTML=`
             <h2  id="teams-header"  class="font-livvic text-[#fff] text-[2rem] font-bold text-center mt-[-0.5rem]">Meet the TeaM</h2>
             <div id="teams-profile"  class="laptop:gap-8 laptop:w-[69.375rem] laptop:h-[38rem] laptop:grid laptop:grid-cols-3 tablet:grid tablet:grid-cols-2 tablet:h-[58.6875rem] tablet:gap-x-4 tablet:gap-y-2 mobile:flex mobile:flex-col mobile:gap-y-[1.68rem] chldren:w-[20.4375rem] children:h-[17.5625rem] ">
                 <div>
-                    <div  class=" bg-sacramento font-livvic h-[15.8125rem] flex flex-col mobile:teamscontainer profile">
+                    <div  class=" bg-sacramento font-livvic h-[15.8125rem] flex flex-col mx-auto mobile:teamscontainer profile">
                         <img class="avatar profile-image" src='../assets/avatar-nikita.jpg'/>
                         <div class="text-center  mobile:mt-4">
                             <h3 class=" text-rapture-blue avatarText profile-name">Nikita Marks</h3>
                             <p class="font-italic text-[#fff] profile-role">Founder & CEO</p>
+                            <div class="mobile:flex mobile:flex-row mx-auto justify-center mt-3 mobile:mt-4 mobile:gap-4 laptop:flex laptop:flex-row laptop:gap-4">
+                                 <a class="twitter-link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
+                                    <path fill="#FFF" d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"/>
+                                </svg></a>
+                                <a class="linkedIn-link"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+                                    <path fill="#FFF" d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"/>
+                                </svg></a>
+                            </div>
                         </div>
                     </div>
                     <div class="avatarplus hover:rapture-blue hover:cursor-pointer">
@@ -44,6 +52,14 @@ document.querySelector('#teams-section').innerHTML=`
                         <div class="text-center  mobile:mt-4">
                             <h3 class=" text-rapture-blue avatarText profile-name">Cristian Duncan</h3>
                             <p class="font-italic text-[#fff]  profile-role">Co-founder & COO</p>
+                            <div class="mobile:flex mobile:flex-row mx-auto justify-center mt-3 mobile:mt-4 mobile:gap-4 laptop:flex laptop:flex-row laptop:gap-4">
+                                <a class="twitter-link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
+                                    <path fill="#FFF" d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"/>
+                                </svg></a>
+                                <a class="linkedIn-link"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+                                     <path fill="#FFF" d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"/>
+                                </svg></a>
+                            </div>
                         </div>
                     </div>
                     <div class="avatarplus  hover:fill-rapture-blue hover:cursor-pointer">
@@ -59,6 +75,14 @@ document.querySelector('#teams-section').innerHTML=`
                         <div class="text-center  mobile:mt-4">
                             <h3 class=" text-rapture-blue avatarText profile-name ">Cruz Hamer</h3>
                             <p class="font-italic text-[#fff] profile-role">Co-founder & CTO</p>
+                            <div class="mobile:flex mobile:flex-row mx-auto justify-center mt-3 mobile:mt-4 mobile:gap-4 laptop:flex laptop:flex-row laptop:gap-4">
+                                 <a class="twitter-link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
+                                    <path fill="#FFF" d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"/>
+                                </svg></a>
+                                <a class="linkedIn-link"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+                                     <path fill="#FFF" d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"/>
+                                </svg></a>
+                            </div>
                         </div>
                     </div>
                     <div class="avatarplus  hover:fill-rapture-blue hover:cursor-pointer">
@@ -73,7 +97,15 @@ document.querySelector('#teams-section').innerHTML=`
                         <img class="avatar profile-image" src='../assets/avatar-drake.jpg'/>
                         <div class="text-center  mobile:mt-4">
                             <h3 class=" text-rapture-blue avatarText profile-name">Drake Heaton</h3>
-                            <p class="font-italic text-[#fff]">Business Developement Lead</p>
+                            <p class="font-italic text-[#fff] profile-role">Business Developement Lead</p>
+                            <div class="mobile:flex mobile:flex-row mx-auto justify-center mt-3 mobile:mt-4 mobile:gap-4 laptop:flex laptop:flex-row laptop:gap-4">
+                                 <a class="twitter-link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
+                                    <path fill="#FFF" d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"/>
+                                </svg></a>
+                                <a class="linkedIn-link"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+                                    <path fill="#FFF" d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"/>
+                                </svg></a>
+                            </div>
                         </div>
                     </div>
                     <div class="avatarplus  hover:fill-rapture-blue hover:cursor-pointer">
@@ -116,6 +148,18 @@ document.querySelector('#teams-section').innerHTML=`
                         <div class="text-center  mobile:mt-4">
                             <h3 class=" text-rapture-blue avatarText profile-name">Griffin Wise</h3>
                             <p class="font-italic text-[#fff] profile-role">Lead Marketing</p>
+                            <div class="mobile:flex mobile:flex-row mx-auto justify-center mt-3 mobile:mt-4 mobile:gap-4 laptop:flex laptop:flex-row laptop:gap-4">
+                                <a class="twitter-link">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
+                                        <path fill="#FFF" d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"/>
+                                    </svg>
+                                </a>
+                                <a class="linkedIn-link">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+                                        <path fill="#FFF" d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"/>
+                                    </svg>
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <div class="avatarplus  hover:fill-rapture-blue hover:cursor-pointer">
@@ -131,6 +175,18 @@ document.querySelector('#teams-section').innerHTML=`
                         <div class="text-center  mobile:mt-4">
                             <h3 class=" text-rapture-blue avatarText profile-name">Aden Allen</h3>
                             <p class="font-italic text-[#fff] profile-role">Head of Talent</p>
+                            <div class="mobile:flex mobile:flex-row mx-auto justify-center mt-3 mobile:mt-4 mobile:gap-4 laptop:flex laptop:flex-row laptop:gap-4">
+                                <a target="_blank" class="twitter-link">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
+                                        <path fill="#FFF" d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"/>
+                                    </svg>
+                                </a>
+                                <a target="_blank" class="linkedIn-link">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+                                        <path fill="#FFF" d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"/>
+                                    </svg>
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <div class="avatarplus hover:bg-rapture-blue hover:cursor-pointer">
